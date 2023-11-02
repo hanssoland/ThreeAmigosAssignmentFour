c1483f9c37fead59574f77862e42826a1bba9d70
Jaime Garza <jaime.f.garza2@gmail.com>
2023-11-01 19:14:15 -0500

    Implement Module B

--- ImageLab/ImageProcessor.swift
+++ ImageLab/ImageProcessor.swift
@@ -2,34 +2,111 @@
 //  ImageProcessor.swift
 //  ImageLab
 //
-//  Created by Bryce Shurts on 11/1/23.
+//  Created by Bryce Shurts and Jaime Garza on 11/1/23.
 //  Copyright © 2023 Eric Larson. All rights reserved.
 //
 
 import Foundation
 import CoreImage
 
-class imageProcessor {
+class ImageProcessor {
+    //MARK: Temporary Method
+    func simulatePPG() -> [Float] {
+        // Simulate a PPG signal for testing purposes
+        var simulatedPPG: [Float] = []
+        for _ in 0..<60 {
+            let randomValue = Float.random(in: 0.5...1.0) // Simulated PPG values between 0.5 and 1.0
+            simulatedPPG.append(randomValue)
+        }
+        return simulatedPPG
+    }
     
-    //MARK: Class Properties
-    var data: imageData! = nil
+    // MARK: Class Properties
+    private var colorData: ImageData! = nil
+    public var bpmData: BPMData! = nil
     
-    struct imageData {
-        
+    struct ImageData {
         public var bufferPos: Int = 0
         public var red: [Float?] = [Float?](repeating: nil, count: 60)
         public var green: [Float?] = [Float?](repeating: nil, count: 60)
         public var blue: [Float?] = [Float?](repeating: nil, count: 60)
     }
     
+    struct BPMData {
+        public var bufferPos: Int = 0
+        public var bpms: [Int] = [Int](repeating: 0, count: 30)
+    }
+    
     init() {
-        self.data = imageData()
+        self.colorData = ImageData()
+        self.bpmData = BPMData()
     }
 
     func computeBPM(inputImage: CIImage) -> String {
         
-        // Grab RGB color info
-            
-        return "Test"
+        if inputImage.colorSpace {
+            // case rgb = 1
+            if inputImage.colorSpace.model == 1 {
+                
+                // If we have a newly filled buffer
+                if self.colorData.bufferPos >= 60 {
+                    self.colorData.bufferPos = 0
+                    
+                    //Compute the BPM
+                    let bpm: Int = 0
+                    let numPeaks: Int = 0
+                    let avgColor: [Float?] =  [Float?](repeating: nil, count: 60)
+                    
+                    // Find the best colors for the PPG process and average them for each entry (33.3ms worth of data)
+                    for index in 0...59 {
+                        // value range: [0, 1.3333]
+                        avgColor[index] = (self.colorData.red[index] * 1 + self.colorData.green * 2 + self.colorData.blue * 1)/3
+                    }
+                    
+                    // Find any peaks in a 2 second range
+                    for index in 1...58 {
+                        // Check if we have a local peak
+                        if avgColor[index] > avgColor[index - 1] + 0.0 && avgColor[index] > avgColor[index + 1] + 0.0 {
+                            // Check if the local peak is a potential global peak
+                            if avgColor[index] > 0.5 {
+                                numPeaks++
+                            }
+                        }
+                    }
+                    
+                    // Multiply the number of peaks by 30 to approximate the user's heart's BPM
+                    bpm = numPeaks * 30
+                    
+                    if self.bpmData.bufferPos >= 30 {
+                        self.bpmData.bufferPos = 0
+                    }
+                    self.bpmData.bpms[self.bpmData.bufferPos] = bpm
+                    self.bpmData.bufferPos += 1
+                }
+                
+                self.colorData.red[bufferPos] = inputImage.colorSpace?.colorTable[0]
+                self.colorData.green[bufferPos] = inputImage.colorSpace?.colorTable[1]
+                self.colorData.blue[bufferPos] = inputImage.colorSpace?.colorTable[2]
+                self.colorData.bufferPos += 1
+            }
+            else {
+               print("Colorspace's model is not in RGB mode")
+            }
+        }
+        else {
+            print("Color space does not exist for CIImage")
+        }
+        inputImage.colorSpace?.colorTable
+        inputImage.colorSpace?.model
+
+        // Update the PPG graph with the simulated data
+        // Assuming you have access to the PPGGraphView instance here, call the update function
+        // e.g., self.ppgGraphView.update(with: simulatedPPG)
+
+        // For actual PPG signal extraction, replace the simulated data with your PPG extraction logic.
+
+        // Compute the BPM and return it as a string
+        return String(bpm)
     }
 }
+
--- ImageLab/PPGViewController.swift
+++ ImageLab/PPGViewController.swift
@@ -2,28 +2,30 @@
 //  PPGViewController.swift
 //  ImageLab
 //
-//  Created by Bryce Shurts on 11/1/23.
+//  Created by Bryce Shurts and Jaime Garza on 11/1/23.
 //  Copyright © 2023 Eric Larson. All rights reserved.
 //
 
 import UIKit
 import MetalKit
 import AVFoundation
+import CoreImage
 
 class PPGViewController: UIViewController {
     
-    //MARK: Class Properties
+    // MARK: Class Properties
     var cameraHandler: VisionAnalgesic! = nil
-    var processor: imageProcessor! = nil
+    var processor: ImageProcessor! = nil
     
-    //MARK: View Outlets
+    // MARK: View Outlets
     @IBOutlet weak var cameraView: MTKView!
     @IBOutlet weak var bpmText: UILabel!
+    @IBOutlet weak var ppgGraphView: PPGGraphView! // Add a UIView for the PPG graph
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.processor = imageProcessor()
+        self.processor = ImageProcessor()
         
         self.cameraHandler = VisionAnalgesic(view: self.cameraView)
         self.cameraHandler.setCameraPosition(position: AVCaptureDevice.Position.back)
@@ -31,18 +33,48 @@ class PPGViewController: UIViewController {
         
         if !self.cameraHandler.isRunning {
             _ = self.cameraHandler.turnOnFlashwithLevel(0.5)
-            self.cameraHandler.setFPS(desiredFrameRate: 60.0)
+            self.cameraHandler.setFPS(desiredFrameRate: 30.0)
             self.cameraHandler.start()
         }
     }
     
     func processImage(input: CIImage) -> CIImage {
         
-        // Draw the graph here
-        
         // Compute the BPM and update the view
         bpmText.text = self.processor.computeBPM(inputImage: input)
+        
+        // Update the PPG graph with the simulated data
+        self.ppgGraphView.update(with: self.processor.bpmData.bpms)
 
         return input
+    }
+
+class PPGGraphView: UIView {
+    
+    //MARK: Class Properties
+    var ppgData: [Float] = []
+
+    override func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+
+        context.clear(rect)
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.blue.cgColor)
+
+        for (index, value) in ppgData.enumerated() {
+            let x = CGFloat(index) * (rect.width / CGFloat(ppgData.count - 1))
+            let y = rect.height - (CGFloat(value) * rect.height)
+            if index == 0 {
+                context.move(to: CGPoint(x: x, y: y))
+            } else {
+                context.addLine(to: CGPoint(x: x, y: y))
+            }
+        }
+        context.strokePath()
+    }
+
+    func update(with ppgSignal: [Int]) {
+        ppgData = ppgSignal
+        setNeedsDisplay() // Trigger a redraw
     }
 }
